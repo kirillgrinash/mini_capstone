8e875df2c4815fa319259b0a9e356314c3845fa9
kirill grinash <kirillgrinash@gmail.com>
2020-09-01 19:53:35 -0500

    add health and mana routes with json

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,6 +1,16 @@
 class Api::ProductsController < ApplicationController
   def product
-    @product = Product.first
+    @products = Product.all
     render "product.json.jb"
+  end
+
+  def health_potion
+    @health_potion = Product.find_by(name: "Health Potion")
+    render "health_potion.json.jb"
+  end
+
+  def mana_potion
+    @mana_potion = Product.find_by(name: "Mana Potion")
+    render "mana_potion.json.jb"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/all_product" => "products#product"
+    get "/health_potion" => "products#health_potion"
+    get "/mana_potion" => "products#mana_potion"
   end
 end
