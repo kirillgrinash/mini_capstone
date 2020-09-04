986325c2b052530bdaca3a61748efc5d1a972acb
kirill grinash <kirillgrinash@gmail.com>
2020-09-03 21:42:57 -0500

    add patch, add delete

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -17,6 +17,23 @@ class Api::ProductsController < ApplicationController
       description: params[:description],
     )
     @product.save
-    render "create.json.jb"
+    render "show.json.jb"
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+
+    @product.name = params[:name] || @product.name
+    @product.price = params[:price] || @product.price
+    @product.image_url = params[:image_url] || @product.image_url
+    @product.description = params[:description] || @product.description
+    @product.save
+    render "show.json.jb"
+  end
+
+  def destroy
+    product = Product.find_by(id: params[:id])
+    product.destroy
+    render json: { message: "Product has been removed"}
   end
 end
