eed70e3d63911710e5116ae27a5efaf0ae9db7a0
kirill grinash <kirillgrinash@gmail.com>
2020-09-06 12:00:02 -0500

    add  models and show.json

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,17 @@
 class Product < ApplicationRecord
+  def friendly_update_at
+    updated_at.strftime("%B%e, %Y")
+  end
+
+  def is_discounted?
+    price <= 30
+  end
+
+  def tax
+    price * 0.09
+  end
+
+  def total
+    price + tax
+  end
 end
