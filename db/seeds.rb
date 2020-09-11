9e7404ee01ef853d16999429232bad90722b1ba2
kirill grinash <kirillgrinash@gmail.com>
2020-09-10 19:40:43 -0500

    add suppliers table, add column to products table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Product.create(name: "health potion", price: 50, description: "recovers a small portion of your health")
-Product.create(name: "mana potion", price: 25, description: "recovers a small portion of your mana")
-Product.create(name: "health buff", price: 75, description: "buffs your health")
+#Product.create(name: "health potion", price: 50, description: "recovers a small portion of your health")
+#Product.create(name: "mana potion", price: 25, description: "recovers a small portion of your mana")
+#Product.create(name: "health buff", price: 75, description: "buffs your health")
+Supplier.create(name: "Wizard's Chest", email: "WIZ@gmail.com", phone_number: "800-555-1111")
+Supplier.create(name: "Discount Stuff", email: "discounts.stuff@gmail.com", phone_number: "999-103-1478")
+Supplier.create(name: "Mom's", email: "futurama@gmail.com", phone_number: "222-333-4444")
